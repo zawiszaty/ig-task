f397e11bc7d477fef1d810cd536b271a1476ade2
Marek Rode <marek.rode@ingenious.build>
2023-08-21 11:02:34 +0200

    fix container

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd ./docker/
 
-docker compose up -d
+docker compose up --build --remove-orphans -d
 docker compose run workspace composer install
 docker compose run workspace cp -n .env.example .env
+docker compose run workspace php artisan migrate:fresh --seed
